f57a75576d0e549dad6712a546dfe05267e7fe90
camieuxgoofy <tarsisiusadhi@gmail.com>
2023-03-13 18:52:58 +0700

    refactor: upload image and store it to public

--- app/Http/Controllers/ArtistAlbumController.php
+++ app/Http/Controllers/ArtistAlbumController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\ArtistAlbum;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 use Inertia\Inertia;
 
@@ -19,23 +20,22 @@ class ArtistAlbumController extends Controller
         Validator::make($request->all(), [
             'album_title' => 'required',
             'album_release_date' => 'required',
-            'album_art' => 'required',
+            'album_art' => 'required|mimes:jpeg,jpg,png',
             'album_artist_name' => 'required',
             'album_price' => 'required',
         ])->validate();
 
+        $input = $request->all();
 
-        $eFile = request()->file('album_art');
-        $fileName = $eFile->getClientOriginalName();
-        $request->$eFile->move(public_path('uploads'), $fileName);
+        if ($image = $request->file('album_art')) {
+            $destinationPath = 'images/';
+            $profileImage = date('YmdHis') . "." . $image->getClientOriginalExtension();
+            $image->move($destinationPath, $profileImage);
+            $input['image'] = "$profileImage";
+        }
 
-        ArtistAlbum::create([
-            'album_title' => $request->album_title,
-            'album_release_date' => $request->album_release_date,
-            'album_art' => $fileName,
-            'album_artist_name' => $request->album_artist_name,
-            'album_price' => $request->album_price,
-        ]);
+        ArtistAlbum::create($input);
+
         return redirect()->route('artistDashboard');
     }
 }
--- resources/js/Pages/Menus/Artist/AddAlbum.jsx
+++ resources/js/Pages/Menus/Artist/AddAlbum.jsx
@@ -1,36 +1,9 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head, usePage } from "@inertiajs/react";
-import { useState } from "react";
-import { router } from "@inertiajs/react";
+import { Head } from "@inertiajs/react";
+import FormAddAlbum from "./FormAddAlbum";
 import "@/../css/main.css";
 
 export default function Dashboard(props) {
-    const [data, setData] = useState({
-        album_title: "",
-        album_release_date: "",
-        album_art: "",
-        album_artist_name: "",
-        album_price: "",
-    });
-
-    function handleChange(e) {
-        const key = e.target.id;
-        const value = e.target.value;
-        setData((data) => ({
-            ...data,
-            [key]: value,
-        }));
-        // console.log(e.target.files);
-
-        /* Uncaught TypeError: Cannot read properties of null (reading '0') */
-        // setCoverArtPreview(URL.createObjectURL(e.target.files[0]));
-    }
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        router.post("store", data);
-    }
-
     return (
         <AuthenticatedLayout
             auth={props.auth}
@@ -62,7 +35,7 @@ export default function Dashboard(props) {
                                 </div>
                                 <div className="relative md:w-56 w-full over overflow-hidden">
                                     <p className="pl-1 text-xl">
-                                        {data.album_title}
+                                        {/* {data.album_title} */}
                                     </p>
                                     <span className="pl-1 absolute bottom-5 text-xs">
                                         by:
@@ -70,7 +43,7 @@ export default function Dashboard(props) {
                                     </span>
                                     <span className="pl-1 text-xs text-gray-500 absolute md:block bottom-1">
                                         Relase date:
-                                        {data.album_art}
+                                        {/* {data.album_art} */}
                                     </span>
                                 </div>
                             </div>
@@ -88,100 +61,10 @@ export default function Dashboard(props) {
                                     Lorem ipsum is placeholder text.
                                 </p>
                             </div>
-                            <form
-                                className="mt-8 space-y-3"
-                                name="createForm"
-                                onSubmit={handleSubmit}
-                                encType="multipart/form-data"
-                            >
-                                <div className="grid grid-cols-1 space-y-2">
-                                    <label className="text-sm font-bold text-gray-500 tracking-wide">
-                                        Album Title
-                                    </label>
-                                    <input
-                                        id="album_title"
-                                        className="text-base p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
-                                        type="text"
-                                        name="album_title"
-                                        value={data.album_title}
-                                        placeholder="Your Album Title"
-                                        onChange={handleChange}
-                                    />
-                                </div>
-                                <div className="flex flex-col md:flex-row space-x-1 space-y-2">
-                                    <div className="grid grid-cols-1 space-y-2 md:w-1/2">
-                                        <label className="text-sm font-bold text-gray-500 tracking-wide">
-                                            Release Date
-                                        </label>
-                                        <input
-                                            id="album_release_date"
-                                            className="text-base p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
-                                            type="date"
-                                            name="album_release_date"
-                                            value={data.album_release_date}
-                                            placeholder="Your Album Title"
-                                            onChange={handleChange}
-                                        />
-                                    </div>
-                                    <div className="grid grid-cols-1 space-y-2 md:w-2/3">
-                                        <label className="text-sm font-bold text-gray-500 tracking-wide">
-                                            Album Art
-                                        </label>
-                                        <input
-                                            id="album_art"
-                                            className="relative text-base block flex-auto cursor-pointer rounded-lg border border-solid border-gray-200 bg-clip-padding px-3 font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:cursor-pointer file:overflow-hidden file:rounded-md file:border-0 file:border-solid file:border-inherit file:bg-turquoise file:px-3 file:py-2 file:text-blueNavy file:transition file:duration-150 file:ease-in-out file:[margin-inline-end:0.75rem] hover:file:bg-green-300 focus:shadow-primary focus:outline-none"
-                                            type="file"
-                                            name="album_art"
-                                            value={data.album_art}
-                                            onChange={handleChange}
-                                        />
-                                    </div>
-                                </div>
-                                <div className="grid grid-cols-1 space-y-2">
-                                    <label className="text-sm font-bold text-gray-500 tracking-wide">
-                                        Artist Name
-                                    </label>
-                                    <input
-                                        id="album_artist_name"
-                                        className="text-base p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
-                                        type=""
-                                        name="album_artist_name"
-                                        value={data.album_artist_name}
-                                        placeholder="Your Artits Name"
-                                        onChange={handleChange}
-                                    />
-                                </div>
 
-                                <div className="grid grid-cols-1 space-y-2">
-                                    <label className="text-sm font-bold text-gray-500 tracking-wide">
-                                        Price
-                                    </label>
-                                    <div className="flex border rounded-lg">
-                                        <p className="self-center pl-2 text-gray-500">
-                                            Rp.
-                                        </p>
-                                        <input
-                                            id="album_price"
-                                            className="text-base w-full p-2 border-0 border-gray-300 rounded-lg focus:outline-none focus:shadow-none"
-                                            type="number"
-                                            name="album_price"
-                                            value={data.album_price}
-                                            placeholder="enter zero or more (e.g., 0, 10000"
-                                            onChange={handleChange}
-                                        />
-                                    </div>
-                                </div>
+                            <FormAddAlbum />
 
-                                <div>
-                                    <button
-                                        type="submit"
-                                        className="my-5 w-full flex justify-center bg-turquoise text-blueNavy p-4  rounded-full tracking-wide
-                                    font-semibold  focus:outline-none focus:shadow-outline hover:bg-[#00b896] shadow-lg cursor-pointer transition ease-in duration-200"
-                                    >
-                                        Next
-                                    </button>
-                                </div>
-                            </form>
+                            {/* FORM */}
                         </div>
                     </div>
                 </div>
