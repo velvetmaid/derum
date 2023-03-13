301e92925975dd462b0200fc23957c20b32a2176
camieuxgoofy <tarsisiusadhi@gmail.com>
2023-03-13 12:43:57 +0700

    feat: album controller store

--- app/Http/Controllers/ArtistAlbumController.php
+++ app/Http/Controllers/ArtistAlbumController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\ArtistAlbum;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Inertia\Inertia;
 
 class ArtistAlbumController extends Controller
@@ -11,5 +12,28 @@ class ArtistAlbumController extends Controller
     public function create()
     {
         return Inertia::render('Menus/Artist/AddAlbum');
+    }
+
+    public function store(Request $request)
+    {
+        Validator::make($request->all(), [
+            'album_title' => ['required'],
+            'album_release_date' => ['required'],
+            'album_art' => ['required'],
+            'album_artist_name' => ['required'],
+            'album_price' => ['required'],
+        ])->validate();
+
+        $image_name = time() . '.' . $request->album_art->extension();
+        $request->file->move(public_path('uploads'), $image_name);
+
+        ArtistAlbum::create([
+            'album_title' => $request->album_title,
+            'album_release_date' => $request->album_release_date,
+            'album_art' => $image_name,
+            'album_artist_name' => $request->album_artist_name,
+            'album_price' => $request->album_price,
+        ]);
+        return redirect()->route('/artistDashboard');
     }
 }
