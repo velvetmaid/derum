8a6c24e10934028a63b48578b84c8084f01a8815
camieuxgoofy <tarsisiusadhi@gmail.com>
2023-03-20 22:08:58 +0700

    feat: add respnsive, get data

--- resources/js/Pages/ComponentContents/PopularSlide.jsx
+++ resources/js/Pages/ComponentContents/PopularSlide.jsx
@@ -1,64 +1,77 @@
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
 import "@/../css/main.css";
+import { useEffect } from "react";
+import { useState } from "react";
 
-export default function PopularSlide() {
+export default function PopularSlide({ posts }) {
+    const [width, setWidth] = useState(window.innerWidth);
+
+    function handleWindowSizeChange() {
+        setWidth(window.innerWidth);
+    }
+    useEffect(() => {
+        window.addEventListener("resize", handleWindowSizeChange);
+        return () => {
+            window.removeEventListener("resize", handleWindowSizeChange);
+        };
+    }, []);
     return (
-        <div className="max-w-7xl mx-auto  h-auto px-6">
+        <div className="max-w-7xl mx-auto px-6">
             <div className="py-2">
-                <Splide
-                    options={{
-                        type: "loop",
-                        drag: "free",
-                        focus: "center",
-                        arrows: false,
-                        pagination: false,
-                        perPage: 4,
-                    }}
-                >
-                    <SplideSlide>
-                        <img
-                            className="px-2 md:px-4"
-                            src="https://www.revolvermag.com/sites/default/files/styles/original_image__844px_x_473px_/public/media/section-media/np_scum.jpg?itok=kPKsGiSd&timestamp=1502731429"
-                            alt="Image 1"
-                        />
-                    </SplideSlide>
-                    <SplideSlide>
-                        <img
-                            className="px-2 md:px-4"
-                            src="https://www.revolvermag.com/sites/default/files/styles/original_image__844px_x_473px_/public/media/section-media/np_scum.jpg?itok=kPKsGiSd&timestamp=1502731429"
-                            alt="Image 2"
-                        />
-                    </SplideSlide>
-                    <SplideSlide>
-                        <img
-                            className="px-2 md:px-4"
-                            src="https://www.revolvermag.com/sites/default/files/styles/original_image__844px_x_473px_/public/media/section-media/np_scum.jpg?itok=kPKsGiSd&timestamp=1502731429"
-                            alt="Image 2"
-                        />
-                    </SplideSlide>
-                    <SplideSlide>
-                        <img
-                            className="px-2 md:px-4"
-                            src="https://www.revolvermag.com/sites/default/files/styles/original_image__844px_x_473px_/public/media/section-media/np_scum.jpg?itok=kPKsGiSd&timestamp=1502731429"
-                            alt="Image 2"
-                        />
-                    </SplideSlide>
-                    <SplideSlide>
-                        <img
-                            className="px-2 md:px-4"
-                            src="https://www.revolvermag.com/sites/default/files/styles/original_image__844px_x_473px_/public/media/section-media/np_scum.jpg?itok=kPKsGiSd&timestamp=1502731429"
-                            alt="Image 2"
-                        />
-                    </SplideSlide>
-                    <SplideSlide>
-                        <img
-                            className="px-2 md:px-4"
-                            src="https://www.revolvermag.com/sites/default/files/styles/original_image__844px_x_473px_/public/media/section-media/np_scum.jpg?itok=kPKsGiSd&timestamp=1502731429"
-                            alt="Image 2"
-                        />
-                    </SplideSlide>
-                </Splide>
+                {width <= 768 ? (
+                    <Splide
+                        options={{
+                            type: "loop",
+                            drag: "free",
+                            focus: "center",
+                            arrows: false,
+                            pagination: false,
+                            perPage: 2,
+                        }}
+                    >
+                        {posts.map((post) => {
+                            return (
+                                <SplideSlide key={post.id} className="">
+                                    <img
+                                        className="px-2 md:px-4 object-cover w-full h-full"
+                                        src={
+                                            "images/content/albums/" +
+                                            post.album_art
+                                        }
+                                        alt={post.album_title}
+                                    />
+                                </SplideSlide>
+                            );
+                        })}
+                    </Splide>
+                ) : (
+                    <Splide
+                        options={{
+                            type: "loop",
+                            drag: "free",
+                            focus: "center",
+                            arrows: false,
+                            pagination: false,
+                            perPage: 4,
+                        }}
+                    >
+                        {posts.map((post) => {
+                            return (
+                                <SplideSlide key={post.id} className="">
+                                    <img
+                                        className="px-2 md:px-4 object-cover w-full h-full"
+                                        src={
+                                            "images/content/albums/" +
+                                            post.album_art
+                                        }
+                                        alt={post.album_title}
+                                    />
+                                </SplideSlide>
+                            );
+                        })}
+                    </Splide>
+                )}
             </div>
         </div>
     );
