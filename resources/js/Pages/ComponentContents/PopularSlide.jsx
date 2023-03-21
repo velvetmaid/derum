17ed1a67a1ac0bdab064580deaf911b306ece5a9
camieuxgoofy <tarsisiusadhi@gmail.com>
2023-03-21 14:25:33 +0700

    feat: update popular slide

--- resources/js/Pages/ComponentContents/PopularSlide.jsx
+++ resources/js/Pages/ComponentContents/PopularSlide.jsx
@@ -19,6 +19,9 @@ export default function PopularSlide({ posts }) {
     return (
         <div className="max-w-7xl mx-auto px-6">
             <div className="py-2">
+                <h1 className="text-2xl font-bold mb-2 text-blueNavy dark:text-yellow-400">
+                    New
+                </h1>
                 {width <= 768 ? (
                     <Splide
                         options={{
@@ -32,16 +35,30 @@ export default function PopularSlide({ posts }) {
                     >
                         {posts.map((post) => {
                             return (
-                                <SplideSlide key={post.id} className="">
-                                    <img
-                                        className="px-2 md:px-4 object-cover w-full h-full"
-                                        src={
-                                            "images/content/albums/" +
-                                            post.album_art
-                                        }
-                                        alt={post.album_title}
-                                    />
+                                <>
+                                    <SplideSlide key={post.id}>
+                                    <div className="flex flex-col w-full h-full">
+                                        <a href="" className="w-full h-full">
+                                            <img
+                                                className="px-2 md:px-4 object-cover w-full h-full relative"
+                                                src={
+                                                    "images/content/albums/" +
+                                                    post.album_art
+                                                }
+                                                alt={post.album_title}
+                                            />
+                                        </a>
+                                        <div className="px-4 flex flex-col -space-y-1">
+                                            <span className="font-robotocondensed">
+                                                {post.album_title}
+                                            </span>
+                                            <span className="font-sofiacondensed text-xs">
+                                                by: {post.album_artist_name}
+                                            </span>
+                                        </div>
+                                    </div>
                                 </SplideSlide>
+                                </>
                             );
                         })}
                     </Splide>
@@ -58,15 +75,27 @@ export default function PopularSlide({ posts }) {
                     >
                         {posts.map((post) => {
                             return (
-                                <SplideSlide key={post.id} className="">
-                                    <img
-                                        className="px-2 md:px-4 object-cover w-full h-full"
-                                        src={
-                                            "images/content/albums/" +
-                                            post.album_art
-                                        }
-                                        alt={post.album_title}
-                                    />
+                                <SplideSlide key={post.id}>
+                                    <div className="flex flex-col w-full h-full">
+                                        <a href="" className="w-full h-full">
+                                            <img
+                                                className="px-2 md:px-4 object-cover w-full h-full relative"
+                                                src={
+                                                    "images/content/albums/" +
+                                                    post.album_art
+                                                }
+                                                alt={post.album_title}
+                                            />
+                                        </a>
+                                        <div className="px-4 flex flex-col -space-y-1">
+                                            <span className="font-robotocondensed">
+                                                {post.album_title}
+                                            </span>
+                                            <span className="font-sofiacondensed text-xs">
+                                                by: {post.album_artist_name}
+                                            </span>
+                                        </div>
+                                    </div>
                                 </SplideSlide>
                             );
                         })}
