b86e7fcae2ab080369b45606f98eba4c2d90a91b
camieuxgoofy <tarsisiusadhi@gmail.com>
2023-03-21 14:21:11 +0700

    feat: add google fonts

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,6 +11,11 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- Google Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&family=Sofia+Sans+Extra+Condensed&family=Stint+Ultra+Condensed&display=swap" rel="stylesheet">
+
         <!-- Scripts -->
         @routes
         @viteReactRefresh
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,9 @@ module.exports = {
             // },
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                robotocondensed: ['Roboto Condensed', 'sans-serif'],
+                sofiacondensed: ['Sofia Sans Extra Condensed', 'sans-serif'],
+                stincondensed: ['Stint Ultra Condensed', 'cursive']
             },
             colors: {
                 'turquoise': {
