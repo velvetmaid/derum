3cc9b19ae79f463b6adb71f58c012d87f6692185
camieuxgoofy <tarsisiusadhi@gmail.com>
2023-03-22 18:23:02 +0700

    feat: update relation

--- app/Models/ArtistAlbum.php
+++ app/Models/ArtistAlbum.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ArtistAlbum extends Model
@@ -28,8 +28,8 @@ class ArtistAlbum extends Model
         return $this->hasMany(ArtistSong::class);
     }
 
-    public function user(): BelongsToMany
+    public function user(): BelongsTo
     {
-        return $this->belongsToMany(User::class);
+        return $this->belongsTo(User::class);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -44,8 +44,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function artist_album(): BelongsToMany
+    public function artist_album(): HasMany
     {
-        return $this->belongsToMany(ArtistAlbum::class);
+        return $this->hasMany(ArtistAlbum::class);
     }
 }
