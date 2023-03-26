ef6324b58f058fdbf698406d2de4a9ae9e9ed223
camieuxgoofy <tarsisiusadhi@gmail.com>
2023-03-26 20:33:17 +0700

    feat(React): Implement dynamic input array and modal functionality

--- resources/js/Pages/Menus/Artist/AddAlbum.jsx
+++ resources/js/Pages/Menus/Artist/AddAlbum.jsx
@@ -1,8 +1,15 @@
 import { Head, useForm } from "@inertiajs/react";
-import Layout from "../../../Layouts/Layout";
+import Layout from "@/Layouts/Layout";
 import "@/../css/main.css";
+import Modal from "@/Components/Modal";
+import { useState } from "react";
 
 export default function AddAlbum(props) {
+    const [showModal, setShowModal] = useState(false);
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
+
     const { data, setData, post, progress, errors } = useForm({
         album_title: "",
         album_release_date: "",
@@ -10,23 +17,60 @@ export default function AddAlbum(props) {
         album_artist_name: "",
         album_price: "",
         album_user_id: props.auth.user.id,
+        songs: [
+            {
+                song_title: "",
+                song_lyric: "",
+                song_file: null,
+            },
+        ],
     });
 
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
         setData({ ...data, [name]: value });
     };
 
-    const handleFileInputChange = (event) => {
-        const file = event.target.files[0];
+    const handleSongInputChange = (e, index) => {
+        const { name, value } = e.target;
+        const songs = [...data.songs];
+        songs[index][name] = value;
+        setData({ ...data, songs });
+    };
+
+    const handleFileInputChange = (e) => {
+        const file = e.target.files[0];
         setData({ ...data, album_art: file });
+    };
+
+    const handleSongFileChange = (e, index) => {
+        const file = e.target.files[0];
+        const songs = [...data.songs];
+        songs[index].song_file = file;
+        setData({ ...data, songs });
+    };
+
+    const addSongInput = () => {
+        setData({
+            ...data,
+            songs: [
+                ...data.songs,
+                { song_title: "", song_lyric: "", song_file: null },
+            ],
+        });
+        handleCloseModal();
+    };
+
+    const removeSongInput = (index) => {
+        const songs = [...data.songs];
+        songs.splice(index, 1);
+        setData({ ...data, songs });
     };
 
     function submit(e) {
         e.preventDefault();
         post("store");
     }
-    // console.log(data);
 
     return (
         <>
@@ -139,7 +183,7 @@ export default function AddAlbum(props) {
                                         <input
                                             id="album_artist_name"
                                             className="text-base p-2 border border-gray-500 dark:bg-blueNavy-dark rounded-lg focus:outline-none focus:border-turquoise"
-                                            type=""
+                                            type="text"
                                             name="album_artist_name"
                                             value={data.album_artist_name}
                                             placeholder="Your Artits Name"
@@ -166,6 +210,146 @@ export default function AddAlbum(props) {
                                             />
                                         </div>
                                     </div>
+
+                                    {/* Modal */}
+                                    <Modal
+                                        show={showModal}
+                                        onClose={handleCloseModal}
+                                    >
+                                        {data.songs &&
+                                            data.songs.map((song, index) => (
+                                                <div
+                                                    className="sm:max-w-lg w-full p-4 bg-white dark:bg-blueNavy-dark rounded-xl z-10 mx-auto"
+                                                    key={index}
+                                                >
+                                                    <div className="grid grid-cols-1 space-y-2">
+                                                        <label className="text-sm font-bold  tracking-wide">
+                                                            Song Title
+                                                        </label>
+                                                        <input
+                                                            className="text-base p-2 border border-gray-500 dark:bg-blueNavy-dark rounded-lg focus:outline-none focus:border-turquoise"
+                                                            type="text"
+                                                            placeholder="Song Title"
+                                                            name="song_title"
+                                                            value={
+                                                                song.song_title
+                                                            }
+                                                            onChange={(e) =>
+                                                                handleSongInputChange(
+                                                                    e,
+                                                                    index
+                                                                )
+                                                            }
+                                                        />
+                                                    </div>
+                                                    <div className="grid grid-cols-1 space-y-2">
+                                                        <label className="text-sm font-bold  tracking-wide">
+                                                            Song Lyric
+                                                        </label>
+                                                        <textarea
+                                                            rows={2}
+                                                            className="text-base p-2 border border-gray-500 dark:bg-blueNavy-dark rounded-lg focus:outline-none focus:border-turquoise"
+                                                            type="text"
+                                                            placeholder="Write a lyric here..."
+                                                            name="song_lyric"
+                                                            value={
+                                                                song.song_lyric
+                                                            }
+                                                            onChange={(e) =>
+                                                                handleSongInputChange(
+                                                                    e,
+                                                                    index
+                                                                )
+                                                            }
+                                                        />
+                                                    </div>
+                                                    <div className="grid grid-cols-1 space-y-2">
+                                                        <label className="text-sm font-bold tracking-wide">
+                                                            Attach the song file
+                                                        </label>
+                                                        <div className="flex items-center justify-center w-full">
+                                                            <label className="flex flex-col rounded-lg border-4 border-dashed border-gray-400 dark:border-gray-600 w-full h-20 p-10 group text-center cursor-pointer">
+                                                                <div className="h-full w-full text-center flex flex-col items-center justify-center">
+                                                                    <svg
+                                                                        xmlns="http://www.w3.org/2000/svg"
+                                                                        className="w-10 h-10 text-blue-400 group-hover:text-blue-600"
+                                                                        fill="none"
+                                                                        viewBox="0 0 24 24"
+                                                                        stroke="currentColor"
+                                                                    >
+                                                                        <path
+                                                                            strokeLinecap="round"
+                                                                            strokeLinejoin="round"
+                                                                            strokeWidth="2"
+                                                                            d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                                                                        />
+                                                                    </svg>
+                                                                    <p className="pointer-none  ">
+                                                                        <span className="text-sm">
+                                                                            Drag
+                                                                            and
+                                                                            drop
+                                                                        </span>
+                                                                        files
+                                                                        here
+                                                                        <br />
+                                                                        or
+                                                                        <span className="text-blue-600 hover:underline">
+                                                                            {
+                                                                                " select a file "
+                                                                            }
+                                                                        </span>
+                                                                        from
+                                                                        your
+                                                                        computer
+                                                                    </p>
+                                                                </div>
+                                                                <input
+                                                                    type="file"
+                                                                    className="hidden"
+                                                                    name="song_file"
+                                                                    value={undefined}
+                                                                    onChange={(
+                                                                        event
+                                                                    ) =>
+                                                                        handleSongFileChange(
+                                                                            event,
+                                                                            index
+                                                                        )
+                                                                    }
+                                                                />
+                                                            </label>
+                                                        </div>
+                                                    </div>
+                                                    <p className="text-sm">
+                                                        File type: mp3
+                                                    </p>
+                                                    <div className="flex space-x-4">
+                                                        <button
+                                                            type="button"
+                                                            onClick={() =>
+                                                                setShowModal(
+                                                                    false
+                                                                )
+                                                            }
+                                                            className="my-5 w-full flex justify-center bg-[#04ddb4] text-[#0d2758] p-4 rounded-full tracking-wide font-semibold focus:outline-none focus:shadow-outline hover:bg-green-300 shadow-lg cursor-pointer transition ease-in duration-200"
+                                                        >
+                                                            Cancel
+                                                        </button>
+                                                        <button
+                                                            onClick={
+                                                                addSongInput
+                                                            }
+                                                            type="buttin"
+                                                            className="my-5 w-full flex justify-center bg-[#04ddb4] text-[#0d2758] p-4 rounded-full tracking-wide font-semibold focus:outline-none focus:shadow-outline hover:bg-green-300 shadow-lg cursor-pointer transition ease-in duration-200"
+                                                        >
+                                                            Add
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            ))}
+                                    </Modal>
+                                    {/* End Modal */}
 
                                     <div>
                                         {progress && (
@@ -208,11 +392,19 @@ export default function AddAlbum(props) {
                                                     input with number
                                                 </span>
                                             ))}
+
                                         <button
                                             type="submit"
                                             className="my-5 w-full flex justify-center bg-turquoise hover:bg-green-300 dark:bg-white dark:hover:bg-gray-300 text-blueNavy p-4 rounded-full tracking-wide font-semibold focus:outline-none focus:shadow-outline shadow-lg cursor-pointer transition duration-200"
                                         >
-                                            Next To Add Songs
+                                            Submit
+                                        </button>
+                                        <button
+                                            type="button"
+                                            onClick={() => setShowModal(true)}
+                                            className="my-5 w-full flex justify-center bg-turquoise hover:bg-green-300 dark:bg-white dark:hover:bg-gray-300 text-blueNavy p-4 rounded-full tracking-wide font-semibold focus:outline-none focus:shadow-outline shadow-lg cursor-pointer transition duration-200"
+                                        >
+                                            Add Modal Songs
                                         </button>
                                     </div>
                                 </form>
