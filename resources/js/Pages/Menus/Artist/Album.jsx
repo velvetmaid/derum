8d0e3aafab675c2de6320fc11839148a6d911d65
camieuxgoofy <tarsisiusadhi@gmail.com>
2023-03-29 01:52:43 +0700

    fix: key

--- resources/js/Pages/Menus/Artist/Album.jsx
+++ resources/js/Pages/Menus/Artist/Album.jsx
@@ -11,29 +11,26 @@ export default function Album({ posts }) {
                             <div className="-m-1 flex flex-wrap md:-m-2 justify-center">
                                 {posts.map((post) => {
                                     return (
-                                        <>
-                                            <div
-                                                key={post.id}
-                                                // Each child in a list should have a unique "key" prop. Check the render method of `Album`
-                                                className="flex md:w-1/3 flex-wrap"
-                                            >
-                                                <div className="relative w-full p-5">
-                                                    <h1>{post.id}</h1>
+                                        <div
+                                            key={post.id}
+                                            className="flex md:w-1/3 flex-wrap"
+                                        >
+                                            <div className="relative w-full p-5">
+                                                <h1>{post.id}</h1>
 
-                                                    <img
-                                                        className="block h-full w-full rounded-lg object-cover object-center"
-                                                        src={
-                                                            "images/content/albums/" +
-                                                            post.album_art
-                                                        }
-                                                        alt={post.album_title}
-                                                    />
-                                                    <span className="absolute bg-black text-white font-robotocondensed opacity-80 hover:opacity-95 hover:scale-105 transition-all cursor-pointer rounded-lg px-3 left-2 bottom-8 text-md md:text-3xl mx-5">
-                                                        {post.album_title}
-                                                    </span>
-                                                </div>
+                                                <img
+                                                    className="block h-full w-full rounded-lg object-cover object-center"
+                                                    src={
+                                                        "images/albums/thumbnails/thumb_" +
+                                                        post.album_art
+                                                    }
+                                                    alt={post.album_title}
+                                                />
+                                                <span className="absolute bg-black text-white font-robotocondensed opacity-80 hover:opacity-95 hover:scale-105 transition-all cursor-pointer rounded-lg px-3 left-2 bottom-8 text-md md:text-3xl mx-5">
+                                                    {post.album_title}
+                                                </span>
                                             </div>
-                                        </>
+                                        </div>
                                     );
                                 })}
                             </div>
