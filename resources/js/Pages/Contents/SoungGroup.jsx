776ed640ea4ff59ad333dd1c6a60051db6e3e1bd
camieuxgoofy <tarsisiusadhi@gmail.com>
2023-04-06 03:25:35 +0700

    feat: Add and update functions play and pause

--- resources/js/Pages/Contents/SoungGroup.jsx
+++ resources/js/Pages/Contents/SoungGroup.jsx
@@ -3,6 +3,8 @@ import { PauseIcon, PlayIcon } from "@heroicons/react/solid";
 
 export default function SongGroup({ props }) {
     const [audio] = useState(new Audio());
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [currentSongId, setCurrentSongId] = useState(null);
 
     const handlePlay = (song) => {
         const soundUrl =
@@ -10,13 +12,44 @@ export default function SongGroup({ props }) {
         audio.pause();
         audio.src = soundUrl;
         audio.play();
+        setIsPlaying(true);
+        setCurrentSongId(song.id);
+    };
+
+    const handlePause = () => {
+        audio.pause();
+        setIsPlaying(false);
+        setCurrentSongId(null);
+    };
+
+    const getPlayButtonClass = (song) => {
+        if (currentSongId === song.id) {
+            return isPlaying
+                ? "opacity-0 pointer-events-none"
+                : "opacity-100 pointer-events-auto";
+        } else {
+            return "opacity-100 pointer-events-auto";
+        }
+    };
+
+    const getPauseButtonClass = (song) => {
+        if (currentSongId === song.id) {
+            return isPlaying
+                ? "opacity-100 pointer-events-auto"
+                : "opacity-0 pointer-events-none";
+        } else {
+            return "opacity-0 pointer-events-none";
+        }
     };
 
     return (
         <div className="flex flex-wrap mx-auto">
             {props.map((album) => (
-                <div className="flex-2 w-1/5 mx-auto" key={album.id}>
-                    <div className="relative inline-block">
+                <div
+                    className="flex-2 w-2/5 md:w-[25%] mx-auto px-0 md:px-2 my-6 flex flex-col"
+                    key={album.id}
+                >
+                    <div className="relative inline-block group:">
                         <img
                             src={
                                 "images/albums/thumbnails/thumb_" +
@@ -25,15 +58,24 @@ export default function SongGroup({ props }) {
                             alt={album.album_title}
                         />
                         {album.artist_song.length > 0 && (
-                            <div className="absolute w-5 top-[50%] left-[50%]" key={album.artist_song[0].id}>
-                                <PlayIcon onClick={() => handlePlay(album)} />
-                                <PauseIcon onClick={() => audio.pause()} />
+                            <div className="cursor-pointer absolute inset-0 bg-gray-900 bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 text-gray-100">
+                                <PlayIcon
+                                    className={`w-12 cursor-pointer absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ${getPlayButtonClass(
+                                        album
+                                    )}`}
+                                    onClick={() => handlePlay(album)}
+                                />
+                                <PauseIcon
+                                    className={`w-12 cursor-pointer absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ${getPauseButtonClass(
+                                        album
+                                    )}`}
+                                    onClick={handlePause}
+                                />
                             </div>
                         )}
                     </div>
-
-                    <a>{album.album_title}</a>
-                    <a>{album.album_artist_name}</a>
+                    <a className="truncate" href="/">{album.album_title}</a>
+                    <a className="truncate text-gray-600" href="/">{album.album_artist_name}</a>
                 </div>
             ))}
         </div>
