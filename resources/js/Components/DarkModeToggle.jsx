2ae26ad99bb0e43e37e0c964eb6d2ee253daa3d1
camieuxgoofy <tarsisiusadhi@gmail.com>
2023-04-06 22:57:00 +0700

    fix: change switch toggle

--- resources/js/Components/DarkModeToggle.jsx
+++ resources/js/Components/DarkModeToggle.jsx
@@ -1,6 +1,5 @@
+import { MoonIcon, SunIcon } from "@heroicons/react/solid";
 import { useState, useEffect } from "react";
-
-const themes = ["light", "dark"];
 
 export default function DarkModeToggle() {
     const [isMounted, setIsMounted] = useState(false);
@@ -40,43 +39,18 @@ export default function DarkModeToggle() {
     }, []);
 
     return isMounted ? (
-        <div className="flex items-center justify-center pr-[22.5px]">
-            {themes.map((t) => {
-                const checked = t === theme;
-                return (
-                    <button
-                        key={t}
-                        onClick={toggleTheme}
-                        type="button"
-                        className={`${
-                            checked ? "text-gray-100" : "text-gray-900"
-                        }`}
-                    >
-                        <svg
-                            className={`${checked ? "hidden" : ""} w-5 h-5`}
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-                        </svg>
-                        <svg
-                            className={`${checked ? "" : "hidden"} w-5 h-5`}
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-                                fillRule="evenodd"
-                                clipRule="evenodd"
-                            ></path>
-                        </svg>
-                    </button>
-                );
-            })}
+        <div className="flex items-center justify-center">
+            <button
+                onClick={toggleTheme}
+                type="button"
+                className="hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none duration-100"
+            >
+                {theme === "light" ? (
+                    <SunIcon className="w-6" />
+                ) : (
+                    <MoonIcon className="w-6" />
+                )}
+            </button>
         </div>
-    ) : (
-        <div />
-    );
+    ) : null;
 }
