1c760af042367dc69ed23871502900e710816e7e
camieuxgoofy <tarsisiusadhi@gmail.com>
2023-05-12 11:25:14 +0700

    feat: add merch slider

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -5,6 +5,7 @@ import NewAlbumSlider from "@/Pages/Contents/NewAlbumSlider";
 import Layout from "../Layouts/Layout";
 import SearchBar from "./Contents/SearchBar";
 import Footer from "@/Components/Footer";
+import NewMerchSlider from "./Contents/NewMerchSlider";
 
 export default function Welcome(props) {
     const targetRef = useRef(null);
@@ -18,6 +19,7 @@ export default function Welcome(props) {
             <Head title="Home" />
             <Hero targetRef={targetRef} scrollTo={scrollTo} {...props} />
             <NewAlbumSlider props={props.albums} />
+            <NewMerchSlider props={props.merches} />
             <SearchBar ref={targetRef} props={props.albums} />
             <Footer />
         </Layout>
