fa2e7d7c2b98986aa0a9b41e4c683cf4a1157ee9
camieuxgoofy <tarsisiusadhi@gmail.com>
2023-05-18 11:20:21 +0700

    feat: add confirmation delete merch

--- resources/js/Pages/Menus/Artist/EditMerch.jsx
+++ resources/js/Pages/Menus/Artist/EditMerch.jsx
@@ -1,13 +1,20 @@
 import { Head, router, useForm } from "@inertiajs/react";
+import { useState } from "react";
 import { toast } from "react-toastify";
 import Layout from "@/Layouts/Layout";
 import FormFeedback from "./Resources/FormFeedback";
 import PreviewMerch from "./PreviewMerch";
 import "@/../css/main.css";
-import { useState } from "react";
+import Modal from "@/Components/Modal";
 
 export default function EditAlbum(props) {
-    const { data, setData, errors, progress } = useForm({
+    const {
+        data,
+        setData,
+        errors,
+        progress,
+        delete: destroy,
+    } = useForm({
         merch_title: props.merches.merch_title || "",
         merch_category: props.merches.merch_category || "",
         merch_image: props.merches.merch_image || null,
@@ -73,6 +80,25 @@ export default function EditAlbum(props) {
             });
         }
     }
+
+    const [showModalDelete, setShowModalDelete] = useState(false);
+
+    const handleCloseModalDelete = () => {
+        setShowModalDelete(false);
+    };
+
+    const handleConfirmDelete = async () => {
+        await destroy(
+            route("delete-merch", {
+                id: props.merches.id,
+            })
+        );
+        handleCloseModalDelete();
+        toast.success("You have successfully deleted the merch", {
+            position: toast.POSITION.TOP_LEFT,
+            className: "w-5/6 md:w-full dark:bg-gray-800",
+        });
+    };
 
     return (
         <>
@@ -196,21 +222,37 @@ export default function EditAlbum(props) {
                                             />
                                         </div>
                                     </div>
-                                    <label className="relative inline-flex items-center cursor-pointer">
-                                        <input
-                                            type="checkbox"
-                                            value=""
-                                            className="sr-only peer"
-                                            onClick={handleToggle}
-                                            defaultChecked={toggleValue === 1}
-                                        />
-                                        <div className="w-11 h-6 bg-gray-400 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-turquoise dark:peer-checked:bg-blue-900" />
-                                        <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-                                            {data.merch_exists === 1
-                                                ? "Available"
-                                                : "Unavailable"}
-                                        </span>
-                                    </label>
+                                    <div className="flex justify-between">
+                                        <label className="relative inline-flex items-center cursor-pointer">
+                                            <input
+                                                type="checkbox"
+                                                className="sr-only peer"
+                                                onClick={handleToggle}
+                                                defaultChecked={
+                                                    toggleValue === 1
+                                                }
+                                            />
+                                            <div className="w-11 h-6 bg-gray-400 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-turquoise dark:peer-checked:bg-blue-900" />
+                                            <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+                                                {data.merch_exists === 1
+                                                    ? "Available"
+                                                    : "Unavailable"}
+                                            </span>
+                                        </label>
+                                        <button
+                                            type="button"
+                                            onClick={() =>
+                                                setShowModalDelete(true)
+                                            }
+                                            className="relative inline-block px-4 font-medium group"
+                                        >
+                                            <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-[5px] translate-y-[5px] bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                                            <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                                            <span className="relative text-blueNavy group-hover:text-white">
+                                                Delete Merch
+                                            </span>
+                                        </button>
+                                    </div>
 
                                     <div>
                                         <FormFeedback
@@ -230,6 +272,64 @@ export default function EditAlbum(props) {
                     </div>
                 </div>
             </Layout>
+            <Modal show={showModalDelete} onClose={handleCloseModalDelete}>
+                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                    <button
+                        type="button"
+                        onClick={handleCloseModalDelete}
+                        className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+                    >
+                        <svg
+                            aria-hidden="true"
+                            className="w-5 h-5"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                clipRule="evenodd"
+                            ></path>
+                        </svg>
+                        <span className="sr-only">Close modal</span>
+                    </button>
+                    <div className="p-6 text-center">
+                        <svg
+                            aria-hidden="true"
+                            className="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                            ></path>
+                        </svg>
+                        <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                            Are you sure you want to delete this merch?
+                        </h3>
+                        <button
+                            type="button"
+                            onClick={handleConfirmDelete}
+                            className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
+                        >
+                            Yes, I'm sure
+                        </button>
+                        <button
+                            type="button"
+                            onClick={handleCloseModalDelete}
+                            className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                        >
+                            No, cancel
+                        </button>
+                    </div>
+                </div>
+            </Modal>
         </>
     );
 }
