665046834b85e3dee74e335cb2b2eda4f72e51df
camieuxgoofy <tarsisiusadhi@gmail.com>
2023-05-21 17:06:54 +0700

    bruh

--- resources/js/Layouts/AuthLayout.jsx
+++ resources/js/Layouts/AuthLayout.jsx
@@ -2,7 +2,7 @@ export default function AuthLayout({ children }) {
     return (
         <div className="bg-dots-darker text-blueNavy bg-center bg-blue-50 dark:bg-dots-lighter dark:bg-gray-900 dark:text-gray-100 transition-all duration-300 ease-in-out">
             <div className="flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
-                <div className="w-full sm:max-w-md px-6 py-14 shadow-md overflow-hidden sm:rounded-lg">
+                <div className="w-full px-6 py-14 shadow-md overflow-hidden sm:rounded-lg">
                     {children}
                 </div>
             </div>
