5afffb14fa35d1cab227a80bed0371b964555c66
camieuxgoofy <tarsisiusadhi@gmail.com>
2023-05-23 19:58:34 +0700

    feat: config api key

--- config/midtrans.php
+++ config/midtrans.php
@@ -4,4 +4,5 @@ return [
     'merchant_id' => env('MIDTRANS_MERCHANT_ID'),
     'client_key' => env('MIDTRANS_CLIENT_KEY'),
     'server_key' => env('MIDTRANS_SERVER_KEY'),
+    'rajaongkir_key' => env('RAJAONGKIR_KEY'),
 ];
