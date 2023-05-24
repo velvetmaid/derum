fdd3ec01b244cffa0628bdb8f121706f96651485
camieuxgoofy <tarsisiusadhi@gmail.com>
2023-05-24 14:30:31 +0700

    feat: add address user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->string('name');
             $table->string('role')->nullable()->default('fan');
             $table->string('email')->unique();
+            $table->string('city')->nullable();
+            $table->text('address')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
