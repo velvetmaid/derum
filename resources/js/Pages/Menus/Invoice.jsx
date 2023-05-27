20e0d12798d96f9a5f612a088e5a322acd725efb
camieuxgoofy <tarsisiusadhi@gmail.com>
2023-05-27 17:30:52 +0700

    feat: add qty invoice

--- resources/js/Pages/Menus/Invoice.jsx
+++ resources/js/Pages/Menus/Invoice.jsx
@@ -1,7 +1,9 @@
 import Layout from "@/Layouts/Layout";
 import { Head, Link } from "@inertiajs/react";
+import moment from "moment-timezone";
 
 export default function Invoice(props) {
+    moment.tz.add("Asia/Jakarta|WIB +07|-7u -70|0101|1nIl0 1zu0");
     console.log(props);
     return (
         <>
@@ -9,30 +11,11 @@ export default function Invoice(props) {
                 <Head title={props.auth.user.name + " Checkout Page"} />
                 <div className="rounded-3xl mb-10 px-6">
                     <h1 className="text-2xl text-center md:text-left md:text-3xl font-bold mb-8">
-                        Welcome
-                        <span className="bg-[#04ddb4] dark:bg-[#3e607b] text-[#0d2758] dark:text-gray-100 italic px-2">
-                            {props.auth.user.name}
-                        </span>
-                        Customer invoice.
+                        Customer invoice
                     </h1>
                     <div className="flex items-center justify-between">
                         <div className="flex items-stretch"></div>
                         <div className="flex items-center gap-x-2 mb-8 md:mb-0">
-                            <Link
-                                href={route("invoice")}
-                                className="inline-flex items-center justify-center h-9 px-3 rounded-xl border hover:border-gray-700 dark:border-gray-900 dark:hover:border-gray-100 transition"
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="1em"
-                                    height="1em"
-                                    fill="currentColor"
-                                    className="bi bi-chat-fill"
-                                    viewBox="0 0 16 16"
-                                >
-                                    <path d="M8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6-.097 1.016-.417 2.13-.771 2.966-.079.186.074.394.273.362 2.256-.37 3.597-.938 4.18-1.234A9.06 9.06 0 0 0 8 15z" />
-                                </svg>
-                            </Link>
                             <Link
                                 href={route("checkout-page")}
                                 className="inline-flex items-center justify-center h-9 px-5 rounded-xl bg-gray-900 text-gray-300 dark:border-white border hover:text-white text-sm font-semibold transition"
@@ -67,105 +50,117 @@ export default function Invoice(props) {
                             </div>
                         </div>
                     </div>
-                    {props.invoices.map((post, index) => (
-                        <div
-                            key={index}
-                            className="shadow-lg dark:shadow-gray-700 bg-blue-100 md:mx-auto mx-4 border rounded-md overflow-hidden text-gray-700 mb-0.5 h-30"
-                        >
-                            <div className="flex flex-wrap p-3 border-l-8 border-cyan-700 dark:border-cyan-900">
-                                <div className="space-y-1 border-r-2 pr-3">
-                                    <div className="text-sm leading-5 font-semibold">
-                                        <span className="text-xs leading-4 font-normal text-gray-500">
-                                            {" "}
-                                            Release #
-                                        </span>{" "}
-                                        {post.id}
-                                    </div>
-                                    <div className="text-sm leading-5 font-semibold">
-                                        <span className="text-xs leading-4 font-normal text-gray-500 pr">
-                                            {" "}
-                                            Product #
-                                        </span>{" "}
-                                        {post.invoice_product_name}
-                                    </div>
-                                    <div className="text-sm leading-5 font-semibold">
-                                        {post.created_at}
-                                    </div>
-                                </div>
-                                <div className="flex-1">
-                                    <div className="ml-3 space-y-1 border-r-2 pr-3">
-                                        <div className="text-base leading-6 font-normal">
-                                            {post.invoice_user_name}
-                                        </div>
-                                        <div className="text-sm leading-4 font-normal">
+                    {props.invoices.map((post, index) => {
+                        const utcDateTime = moment.utc(post.created_at);
+
+                        const wibDateTime = utcDateTime.tz("Asia/Jakarta");
+
+                        const wibFormattedDate = wibDateTime.format(
+                            "DD MMM, YYYY hh:mmA"
+                        );
+
+                        return (
+                            <div
+                                key={index}
+                                className="shadow-lg dark:shadow-gray-700 bg-blue-100 md:mx-auto mx-4 border rounded-md overflow-hidden text-gray-700 mb-0.5 h-30"
+                            >
+                                <div className="flex flex-wrap p-3 border-l-8 border-cyan-700 dark:border-cyan-900">
+                                    <div className="space-y-1 border-r-2 pr-3">
+                                        <div className="text-sm leading-5 font-semibold">
                                             <span className="text-xs leading-4 font-normal text-gray-500">
-                                                Courier{" "}
-                                            </span>
-                                            <span className="uppercase">
-                                                {post.invoice_order_courier} -{" "}
-                                                {
-                                                    post.invoice_courier_description
-                                                }
-                                            </span>
-                                        </div>
-                                        <div className="text-sm leading-4 font-normal">
-                                            <span className="text-xs leading-4 font-normal text-gray-500">
-                                                Destination
+                                                {" "}
+                                                Release #
                                             </span>{" "}
-                                            <span>
-                                                {post.invoice_order_city},{" "}
-                                                {post.invoice_order_address}
-                                            </span>
+                                            {post.id}
                                         </div>
-                                    </div>
-                                </div>
-                                <div className="border-r-2 pr-3">
-                                    <div>
-                                        <div className="ml-3 my-3 border-gray-200 border-2 bg-gray-300 p-1">
-                                            <div className="uppercase text-xs leading-4 font-medium">
-                                                Estimated Time
-                                            </div>
-                                            <div className="text-center text-sm leading-4 font-semibold text-gray-800">
-                                                {post.invoice_etd}
-                                            </div>
+                                        <div className="text-sm leading-5 font-semibold">
+                                            <span className="text-xs leading-4 font-normal text-gray-500 pr">
+                                                {" "}
+                                                Product #
+                                            </span>{" "}
+                                            {post.invoice_product_name}
                                         </div>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div className="ml-3 my-5 dark:bg-gray-800 bg-cyan-800 p-1">
-                                        <div className="text-xs leading-4 font-semibold text-center text-blue-100">
-                                            {post.invoice_total_price.toLocaleString(
-                                                "id-ID",
-                                                {
-                                                    style: "currency",
-                                                    currency: "IDR",
-                                                    minimumFractionDigits: 0,
-                                                }
+                                        <div className="text-sm leading-5 font-semibold">
+                                            {post.invoice_type_product ==
+                                            "Digital" ? null : (
+                                                <span className="text-xs leading-4 font-normal text-gray-500 pr">
+                                                    Qty # {post.invoice_qty}
+                                                </span>
                                             )}
                                         </div>
+                                        <div className="text-sm leading-5 font-semibold">
+                                            {wibFormattedDate}
+                                        </div>
+                                    </div>
+                                    <div className="flex-1">
+                                        <div className="ml-3 space-y-1 border-r-2 pr-3">
+                                            <div className="text-base leading-6 font-normal">
+                                                {post.invoice_user_name}
+                                            </div>
+                                            <div className="text-sm leading-4 font-normal">
+                                                <span className="text-xs leading-4 font-normal text-gray-500">
+                                                    Courier{" "}
+                                                </span>
+                                                <span className="uppercase">
+                                                    {post.invoice_order_courier}{" "}
+                                                    -{" "}
+                                                    {
+                                                        post.invoice_courier_description
+                                                    }
+                                                </span>
+                                            </div>
+                                            <div className="text-sm leading-4 font-normal">
+                                                <span className="text-xs leading-4 font-normal text-gray-500">
+                                                    Destination
+                                                </span>{" "}
+                                                <span>
+                                                    {post.invoice_order_city},{" "}
+                                                    {post.invoice_order_address}
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="border-r-2 pr-3">
+                                        <div>
+                                            <div className="ml-3 my-3 border-gray-200 border-2 bg-gray-300 p-1">
+                                                {post.invoice_type_product ==
+                                                "Digital" ? (
+                                                    <div className="uppercase text-xs leading-4 font-medium py-1 px-7">
+                                                        {
+                                                            post.invoice_type_product
+                                                        }
+                                                    </div>
+                                                ) : (
+                                                    <>
+                                                        <div className="uppercase text-xs leading-4 font-medium">
+                                                            Estimated Time
+                                                        </div>
+                                                        <div className="text-center text-sm leading-4 font-semibold text-gray-800">
+                                                            {post.invoice_etd}
+                                                        </div>
+                                                    </>
+                                                )}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className="ml-3 my-5 dark:bg-gray-800 bg-cyan-800 p-1">
+                                            <div className="text-xs leading-4 font-semibold text-center text-blue-100">
+                                                {post.invoice_total_price.toLocaleString(
+                                                    "id-ID",
+                                                    {
+                                                        style: "currency",
+                                                        currency: "IDR",
+                                                        minimumFractionDigits: 0,
+                                                    }
+                                                )}
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                                {/* <div>
-                                    <button className="text-gray-100 rounded-sm my-5 ml-2 focus:outline-none bg-gray-500">
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            className="h-6 w-6"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            stroke="currentColor"
-                                        >
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth="2"
-                                                d="M19 9l-7 7-7-7"
-                                            />
-                                        </svg>
-                                    </button>
-                                </div> */}
                             </div>
-                        </div>
-                    ))}
+                        );
+                    })}
                 </div>
             </Layout>
         </>
