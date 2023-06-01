f2483fc9f8c1b9cc42a961d50882807cbc0210e9
camieuxgoofy <tarsisiusadhi@gmail.com>
2023-06-01 20:04:22 +0700

    style: update sign layout

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -3,15 +3,10 @@ import { Link, useForm } from "@inertiajs/react";
 import AuthLayout from "@/Layouts/AuthLayout";
 import Checkbox from "@/Components/Checkbox";
 import InputError from "@/Components/InputError";
-import InputLabel from "@/Components/InputLabel";
 import PrimaryButton from "@/Components/PrimaryButton";
-import TextInput from "@/Components/TextInput";
 import "@/../css/main.css";
 
-export default function Login({
-    status,
-    /* canResetPassword */ setShowModalLogin,
-}) {
+export default function Login({ status, canResetPassword, setShowModalLogin }) {
     const { data, setData, post, processing, errors, reset } = useForm({
         email: "",
         password: "",
@@ -47,70 +42,71 @@ export default function Login({
                 </div>
             )}
             <h1 className="text-center text-3xl">Sign In</h1>
-            <form className="px-20" onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="email" value="Email" />
-
-                    <TextInput
+            <form className="px-20 space-y-10" onSubmit={submit}>
+                <div className="relative">
+                    <input
                         id="email"
-                        type="email"
                         name="email"
                         value={data.email}
-                        className="mt-1 block w-full dark:bg-[#1f2937]"
-                        autoComplete="username"
+                        placeholder=" "
                         isFocused={true}
                         onChange={handleOnChange}
+                        className="peer h-full w-full border-t-0 border-l-0 border-r-0 border border-gray-300 dark:border-gray-400 border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-700 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                     />
-
+                    <label class="after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-500 transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-700 dark:after:border-gray-200 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-700 dark:peer-focus:text-gray-200 peer-focus:after:scale-x-100 peer-focus:after:border-gray-700 dark:peer-focus:after:border-gray-200 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                        Email
+                    </label>
                     <InputError message={errors.email} className="mt-2" />
                 </div>
 
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-
-                    <TextInput
+                <div className="relative">
+                    <input
                         id="password"
                         type="password"
                         name="password"
                         value={data.password}
-                        className="mt-1 block w-full dark:bg-[#1f2937]"
-                        autoComplete="current-password"
+                        placeholder=" "
                         onChange={handleOnChange}
+                        className="peer h-full w-full border-t-0 border-l-0 border-r-0 border border-gray-300 dark:border-gray-400 border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-700 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                     />
-
+                    <label class="after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-500 transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-700 dark:after:border-gray-200 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-700 dark:peer-focus:text-gray-200 peer-focus:after:scale-x-100 peer-focus:after:border-gray-700 dark:peer-focus:after:border-gray-200 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                        Password
+                    </label>
                     <InputError message={errors.password} className="mt-2" />
                 </div>
 
-                <div className="block mt-4">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            value={data.remember}
-                            onChange={handleOnChange}
-                        />
-                        <span className="ml-2 text-sm">Remember me</span>
-                    </label>
-                </div>
+                <div>
+                    <div className="block mt-4">
+                        <label className="flex items-center">
+                            <Checkbox
+                                name="remember"
+                                value={data.remember}
+                                onChange={handleOnChange}
+                            />
+                            <span className="ml-2 text-sm">Remember me</span>
+                        </label>
+                    </div>
 
-                <div className="flex items-center justify-end mt-4">
-                    {/* {canResetPassword && ( */}
-                    {/* <Link
-                        href={route("password.request")}
-                        className="cursor-pointer underline text-sm hover:text-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                        Forgot your password?
-                    </Link> */}
-                    {/* )} */}
-                    <PrimaryButton
-                        className="ml-4"
-                        type="button"
-                        onClick={() => setShowModalLogin(false)}
-                    >
-                        Back
-                    </PrimaryButton>
-                    <PrimaryButton className="ml-4" disabled={processing}>
-                        Submit
-                    </PrimaryButton>
+                    <div className="flex items-center justify-end mt-4">
+                        {/* {canResetPassword && ( */}
+                        <Link
+                            href={route("password.request")}
+                            className="cursor-pointer underline text-sm hover:text-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        >
+                            Forgot your password?
+                        </Link>
+                        {/* )} */}
+                        <PrimaryButton
+                            className="ml-4"
+                            type="button"
+                            onClick={() => setShowModalLogin(false)}
+                        >
+                            Back
+                        </PrimaryButton>
+                        <PrimaryButton className="ml-4" disabled={processing}>
+                            Submit
+                        </PrimaryButton>
+                    </div>
                 </div>
             </form>
         </AuthLayout>
--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -4,7 +4,6 @@ import AuthLayout from "@/Layouts/AuthLayout";
 import InputError from "@/Components/InputError";
 import InputLabel from "@/Components/InputLabel";
 import PrimaryButton from "@/Components/PrimaryButton";
-import TextInput from "@/Components/TextInput";
 
 export default function Register({ setShowModalLogin, setShowModalRegister }) {
     const { data, setData, post, processing, errors, reset } = useForm({
@@ -40,33 +39,38 @@ export default function Register({ setShowModalLogin, setShowModalRegister }) {
         <AuthLayout>
             <form className="px-20" onSubmit={submit}>
                 <h1 className="text-center text-3xl">Sign Up</h1>
-                <div className="mt-4">
-                    <InputLabel htmlFor="name" value="Name" />
-                    <TextInput
+
+                <div className="relative mt-4">
+                    <input
                         id="name"
                         name="name"
                         value={data.name}
-                        className="mt-1 block w-full dark:bg-[#1f2937]"
-                        autoComplete="name"
-                        isFocused={true}
                         onChange={handleOnChange}
+                        placeholder=" "
                         required
+                        autoComplete=" "
+                        className="peer h-full w-full border-t-0 border-l-0 border-r-0 border border-gray-300 dark:border-gray-400 border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-700 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                     />
+                    <label className="after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-500 transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-700 dark:after:border-gray-200 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-700 dark:peer-focus:text-gray-200 peer-focus:after:scale-x-100 peer-focus:after:border-gray-700 dark:peer-focus:after:border-gray-200 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                        Name
+                    </label>
                     <InputError message={errors.name} className="mt-2" />
                 </div>
 
-                <div className="mt-4">
-                    <InputLabel htmlFor="email" value="Email" />
-                    <TextInput
+                <div className="relative mt-4">
+                    <input
                         id="email"
                         type="email"
                         name="email"
                         value={data.email}
-                        className="mt-1 block w-full dark:bg-[#1f2937]"
-                        autoComplete="username"
                         onChange={handleOnChange}
+                        placeholder=" "
                         required
+                        className="peer h-full w-full border-t-0 border-l-0 border-r-0 border border-gray-300 dark:border-gray-400 border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-700 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                     />
+                    <label className="after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-500 transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-700 dark:after:border-gray-200 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-700 dark:peer-focus:text-gray-200 peer-focus:after:scale-x-100 peer-focus:after:border-gray-700 dark:peer-focus:after:border-gray-200 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                        Email
+                    </label>
                     <InputError message={errors.email} className="mt-2" />
                 </div>
 
@@ -75,11 +79,12 @@ export default function Register({ setShowModalLogin, setShowModalRegister }) {
                     <select
                         name="role"
                         id="role"
-                        className="mt-1 block w-full dark:bg-[#1f2937] rounded-md"
+                        className="peer dark:bg-gray-900 h-full w-full border-t-0 border-l-0 border-r-0 border border-gray-300 dark:border-gray-400 border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all focus:border-none focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                         onChange={handleOnChange}
+                        defaultValue=""
                     >
-                        <option selected disabled>
-                            Please select
+                        <option value="" disabled>
+                            Select an option
                         </option>
                         <option value="fan">Fan</option>
                         <option value="artist">Artist</option>
@@ -87,36 +92,39 @@ export default function Register({ setShowModalLogin, setShowModalRegister }) {
                     <InputError message={errors.role} className="mt-2" />
                 </div>
 
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-                    <TextInput
+                <div className="relative mt-4">
+                    <input
                         id="password"
                         type="password"
                         name="password"
                         value={data.password}
-                        className="mt-1 block w-full dark:bg-[#1f2937]"
                         autoComplete="new-password"
                         onChange={handleOnChange}
+                        placeholder=" "
                         required
+                        className="peer h-full w-full border-t-0 border-l-0 border-r-0 border border-gray-300 dark:border-gray-400 border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-700 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                     />
+                    <label className="after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-500 transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-700 dark:after:border-gray-200 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-700 dark:peer-focus:text-gray-200 peer-focus:after:scale-x-100 peer-focus:after:border-gray-700 dark:peer-focus:after:border-gray-200 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                        Password
+                    </label>
                     <InputError message={errors.password} className="mt-2" />
                 </div>
 
-                <div className="mt-4">
-                    <InputLabel
-                        htmlFor="password_confirmation"
-                        value="Confirm Password"
-                    />
-                    <TextInput
+                <div className="relative mt-4">
+                    <input
                         id="password_confirmation"
                         type="password"
                         name="password_confirmation"
                         value={data.password_confirmation}
-                        className="mt-1 block w-full dark:bg-[#1f2937]"
                         autoComplete="new-password"
                         onChange={handleOnChange}
+                        placeholder=" "
                         required
+                        className="peer h-full w-full border-t-0 border-l-0 border-r-0 border border-gray-300 dark:border-gray-400 border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-700 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                     />
+                    <label className="after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-500 transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-700 dark:after:border-gray-200 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-700 dark:peer-focus:text-gray-200 peer-focus:after:scale-x-100 peer-focus:after:border-gray-700 dark:peer-focus:after:border-gray-200 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                        Confirm Password
+                    </label>
                     <InputError
                         message={errors.password_confirmation}
                         className="mt-2"
